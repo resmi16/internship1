82d58de375e2f78fae168beb57cd94c4b2d7a6f6
resmi <resmimohan16@gmail.com>
2017-06-20 18:30:07 +0530

    listoperation

---
+++ internship1/list.py
@@ -0,0 +1,12 @@
+big_list = ['p','r','o','g','r','a','m','i','z']
+#elements 3rd to 5th
+print(big_list[2:5])
+
+#elements beginning to 4th
+print(big_list[:-5])
+
+# elements 6th to end
+print(big_list[5:])
+
+# elements beginning to end
+print(big_list[:])
