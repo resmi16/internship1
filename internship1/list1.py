4ed836591727f86e758fb04a7302996ccee49c34
resmi <resmimohan16@gmail.com>
2017-06-20 19:00:18 +0530

    list operation1

---
+++ internship1/list1.py
@@ -0,0 +1,27 @@
+#operation on odd list
+odd = [1, 3, 5]
+
+odd.append(7)
+
+# Output: [1, 3, 5, 7]
+print(odd)
+
+odd.extend([9, 11, 13])
+
+# Output: [1, 3, 5, 7, 9, 11, 13]
+print(odd)
+
+#operatin on my_list
+my_list = ['p','r','o','b','l','e','m']
+
+# delete one item
+del my_list[2]
+
+# Output: ['p', 'r', 'b', 'l', 'e', 'm']     
+print(my_list)
+
+# delete multiple items
+del my_list[1:5]  
+
+# Output: ['p', 'm']
+print(my_list)
